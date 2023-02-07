2ba1f6517bad4c87041ffd4d4f56453c87764176
SeungBin Cha <56204009+csb0710@users.noreply.github.com>
2023-02-07 22:43:27 +0900

    1. 게시판 작성, 수정, 삭제
    게시판 무작정 따라하기

---
+++ CSB/board/src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,27 @@
+package com.study.board.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // 클래스가 db에 있는 테이블을 의미한다는 것을 나타냄
+@Data
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String content;
+
+    private String filename;
+
+    private String filepath;
+
+
+}
