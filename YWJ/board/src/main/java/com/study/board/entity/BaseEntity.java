d02c98556f2098862133a4a8f28d26166608da29
wyoon <101500308+Woojun-Yoon@users.noreply.github.com>
2023-02-12 22:08:59 +0900

    Add_service

---
+++ YWJ/board/src/main/java/com/study/board/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.study.board.entity;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+// 시간 정보를 다루는 Entity
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdTime;
+
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime updatedTime;
+}
