98bdf0a460d21a7cf9f800d5dd0e54bd8d050685
lms990108 <103021300+lms990108@users.noreply.github.com>
2023-02-17 15:42:50 +0900

    프론트추가
    메차쿠차

---
+++ LMS/psver1FrontTest/src/main/java/com/study/psver1/Psver1Application.java
@@ -0,0 +1,13 @@
+package com.study.psver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Psver1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Psver1Application.class, args);
+    }
+
+}
---
+++ LMS/psver1FrontTest/src/main/java/com/study/psver1/controller/MemberController.java
@@ -0,0 +1,52 @@
+package com.study.psver1.controller;
+
+import com.study.psver1.dto.MemberDTO;
+import com.study.psver1.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    // 생성자 주입
+    private final MemberService memberService;
+
+    @GetMapping("/member/save")
+    public String saveForm(){return "save";}
+
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO){
+
+        memberService.save(memberDTO);
+
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm(){return "login";}
+
+
+    @PostMapping("/member/login")
+    public String loginForm(@ModelAttribute MemberDTO memberDTO,
+                            HttpSession session, Model model){
+
+       MemberDTO loginResult = memberService.login(memberDTO);
+       if ( loginResult != null){
+           // 성공
+//           session.setAttribute("loginEmail", loginResult.getMemberEmail());
+           return "redirect:/board/list";
+       }else{
+           // 실패
+           model.addAttribute("message", "로그인 실패");
+           model.addAttribute("searchUrl", "/member/login");
+           return "message";
+       }
+
+    }
+}
