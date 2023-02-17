60d62b86131f0fbbd80fd47fe1293b819ed7b35b
lms990108 <103021300+lms990108@users.noreply.github.com>
2023-02-17 15:33:20 +0900

    어떻게 올려야하는지 모르겠어서 새로운 파일 그냥올림
    이거아닌거같은데 아무튼 올림
    
    업데이트 내용
    1. 회원가입/로그인 기능 추가
    2. 홈페이지/회원가입/로그인/게시판목록 페이지 디자인 추가
    
    다음 업데이트 방향
    1. Back
     1.1 회원탈퇴 기능 추가
     1.2 로그아웃 기능 추가
     1.3 로그인한 사용자에게만 게시글 작성기능 제공
     1.4 회원정보에서 작성자 name 추가
     1.5 " 글 작성자 = 로그인 사용자 "의 경우에만 글 수정 및 삭제 기능 추가
    
    2. Front
     되는대로.

---
+++ LMS/psver1Test/src/test/java/com/study/psver1/Psver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.psver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Psver1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
